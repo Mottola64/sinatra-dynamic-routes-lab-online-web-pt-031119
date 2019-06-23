16a40bf960de42edcc68c6d9799d3eb8795d81f5
Michael Mottola <mottola64@gmail.com>
2019-06-23 18:53:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     answer
     end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     @wordone = params[:word1].to_s
     @wordtwo = params[:word2].to_s
     @wordthree = params[:word3].to_s
